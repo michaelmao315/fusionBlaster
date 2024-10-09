37972447b9dbc845a07f9e6f8bfcda74bab7c7a3
“Michael <egz9nj@virginia.edu>
2024-10-09 16:54:28 -0400

    convert dbwriter to sh

--- db.writer.py.2.sh
+++ db.writer.py.2.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-#SBATCH --account=hlilab
-#SBATCH --partition=standard
-#SBATCH --time=0-23:59:59
-#SBATCH --nodes=1
-#SBATCH --cpus-per-task=4
-#SBATCH --mem=36000
 
 input=$1
 genome=$2
@@ -18,22 +12,14 @@ fi
 # Create the output directory if it doesn't exist
 mkdir -p "$OUTPUT_DIR"
 
-# Check genome and set transcriptome accordingly
-if [[ $genome == "grch37" || $genome == "hg19" ]]; then
-    transcriptome=/project/hlilab/sam/transcriptome/grch37.transcriptome.tsv
-elif [[ $genome == "grch38" || $genome == "hg38" ]]; then
-    transcriptome=/project/hlilab/sam/transcriptome/grch38.transcriptome.tsv
-elif [[ $genome == "grcm38" || $genome == "mm10" ]]; then
-    transcriptome=/project/hlilab/sam/transcriptome/grcm38.transcriptome.tsv
-else
-    echo -e "Please Try again with one of the supported genomes below:\n grch37 or hg19\n grch38 or hg38\n grcm38 or mm10\n"
-    exit 1
-fi
-
-# Load the necessary modules
-module load gcc
-module load anaconda
-module load goolf R
+# Check genome
+case $genome in
+    grch37|hg19|grch38|hg38|grcm38|mm10) ;;
+    *)
+        echo "Error: Unsupported genome '$genome'. Supported genomes are: grch37, hg19, grch38, hg38, grcm38, mm10" >&2
+        exit 1
+        ;;
+esac
 
 # Define output files in the output directory
 full_output="$OUTPUT_DIR/full_output.tsv"
@@ -41,7 +27,7 @@ filtered_output="$OUTPUT_DIR/filtered_output.tsv"
 filtered_input="$OUTPUT_DIR/filtered_${input##*/}"
 
 # Run the R script and save the full output in the output directory
-paste <(cat "$input") <(Rscript /project/hlilab/software/fusionBlaster/genome2transcriptome.R $genome <(cut -f2- "$input")) > "$full_output"
+paste <(cat "$input") <(Rscript ./fusionBlaster/genome2transcriptome.R $genome <(cut -f2- "$input")) > "$full_output"
 
 # Filter out intron regions and create filtered output and input files in the output directory
 awk -F'\t' 'BEGIN {OFS="\t"} {
@@ -55,7 +41,7 @@ awk -F'\t' 'BEGIN {OFS="\t"} {
 cut -f6- "$filtered_output" > "$OUTPUT_DIR/python_input.tsv"
 
 # Call your Python script and save its output to refDB.fa in the output directory
-python /project/hlilab/software/fusionBlaster/db.writer.py "$OUTPUT_DIR/python_input.tsv" $genome > "$OUTPUT_DIR/refDB.fa"
+python ./fusionBlaster/db.writer.py "$OUTPUT_DIR/python_input.tsv" $genome > "$OUTPUT_DIR/refDB.fa"
 
 # Clean up temporary files
 # rm -f "$full_output" "python_input.tsv"
